a340ab8c04fa08950b978670a328c18acbef60d3
Douglas W. Palme <dpalme@palmeacres.com>
2018-07-05 17:21:54 -0500

    Initial Commit

---
+++ Per Diem Gold/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Per Diem Gold
+//
+//  Created by Douglas W. Palme on 7/5/18.
+//  Copyright © 2018 Douglas W. Palme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
